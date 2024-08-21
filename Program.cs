40b805c43e2e7da6c273b89adc514323a1d13929
Satwika05 <skondi@gmu.edu>
2024-08-21 10:24:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print numbers from 1 to 100");
+            for (int i=1; i<=100; i++)
+            {
+                
+                Console.WriteLine(i);
+                
+            }
+            Console.WriteLine("Print even numbers between 1 to 100");
+            for (int i = 1; i <= 100; i++)
+            {
+                
+                if (i%2 ==0)
+                {
+                    
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.WriteLine("Print odd numbers between 1 to 100");
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (i % 2 != 0)
+                {
+
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
